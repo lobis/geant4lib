c76d3fa37e2f7d2083281031da7b6d7a1b9ece4d
Luis Obis <luis.antonio.obis@gmail.com>
2021-10-15 15:54:21 +0200

    Added print method for data classes

--- src/TRestGeant4DataEvent.cxx
+++ src/TRestGeant4DataEvent.cxx
@@ -3,3 +3,13 @@
 //
 
 #include "TRestGeant4DataEvent.h"
+
+#include "spdlog/spdlog.h"
+
+void TRestGeant4DataEvent::Print() const {
+    spdlog::warn("Printing information for event ID {}", fEventID);
+
+    for (const auto& track : fTracks) {
+        track.Print();
+    }
+}
--- src/TRestGeant4DataTrack.cxx
+++ src/TRestGeant4DataTrack.cxx
@@ -3,3 +3,14 @@
 //
 
 #include "TRestGeant4DataTrack.h"
+
+#include "spdlog/spdlog.h"
+
+void TRestGeant4DataTrack::Print() const {
+    spdlog::warn(
+        "Track ID: {} - Parent ID: {} - Particle: {} - Creator process: {} - Initial KE: {:0.2f} keV - "
+        "#secondaries: {}",
+        fTrackID, fParentID, fParticleName, fCreatorProcess, fInitialKineticEnergy, fNumberOfSecondaries);
+
+    fSteps.Print();
+}
